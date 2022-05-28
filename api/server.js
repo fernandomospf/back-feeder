2fd0e2cebcf5a0e62b698402c85a7c7a8065bfc0
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-05-28 00:37:40 -0300

    build(server): adding errorMiddleware in a route

--- api/server.js
+++ api/server.js
@@ -4,9 +4,14 @@ const PORT = process.env.PORT || 3000;
 
 const userRoute = require('../src/routes/routeUser');
 
+const errorMiddleware = require('../src/middlewares/errorMiddleware');
+
+
 app.use(express.json());
 
 app.use('/user', userRoute);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`App escutando na porta ${PORT}`);
