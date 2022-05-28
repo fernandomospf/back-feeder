f954c6920162ade0d74a637a6d139e4441d5fecd
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-05-28 00:39:06 -0300

    build(userController): add try catch

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,8 +1,12 @@
 const userService = require('../service/userService');
 
-const create = async (req, res) => {
-  await userService(req.body);
-  return res.status(201).json({ message: 'Sucess create'});
+const create = async (req, res, next) => {
+  try {
+    await userService.create(req.body);
+    return res.status(201).json({ message: 'Sucess create' });
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
