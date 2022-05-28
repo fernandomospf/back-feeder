25a87ff05af542d3835e2b945da2e59ac12a0c99
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-05-28 00:38:37 -0300

    feat(userService): add function with joi response

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,7 +1,17 @@
 const { User } = require('../models');
+const userSchema = require('../schemas/userSchema');
 
-const create = async ({username, token}) => {
-  const request = await User.create({username, token});
+const statusError = (status, message) => ({
+  status,
+  message,
+});
+
+const create = async ({ username, token }) => {
+  const { error } = userSchema.validate({ username, token });
+  if (error) throw statusError(400, error.message);
+
+
+  const request = await User.create({ username, token });
   return request;
 }
 
