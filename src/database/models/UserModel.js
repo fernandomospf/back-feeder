9d952245bb42743fcb47ce6cb1bad9c85c469b13
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-08-06 17:39:52 -0300

    :sparkles: creating model user

---
+++ src/database/models/UserModel.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require("sequelize/types");
+
+class UserModel extends Model {}
+
+UserModel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+  modelName: 'users',
+  sequelize,
+  timestamps: false,
+  underscore: true,
+});
+
+return UserModel;
