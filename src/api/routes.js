03dff736f7fe254b4e5fc86199342ec44e3b3768
Pedro Lima <pedro.oficial1969@gmail.com>
2022-08-22 18:27:05 -0300

    inicio do endpoint login

---
+++ src/api/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.use('/users');
+routes.use('/project');
+
+modules.exports = {
+  routes,
+}
---
+++ src/modules/User/UserService.js
@@ -0,0 +1,5 @@
+class UserService {
+  async loginUser(email, password) {
+    
+  }
+}
