3e0e565a6b039388a85d4327af80f6c4a7fc0861
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-08-22 17:26:59 -0300

    :sparkles: creating project model

---
+++ src/database/models/ProjectModel.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require("sequelize/types");
+const { UserModel } = require("./UserModel");
+
+class ProjectModel extends Model { }
+
+ProjectModel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    gif: {
+      type: DataTypes.BLOB,
+      allowNull: false,
+    },
+    stack: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    module: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficult: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+  modelName: 'projects',
+  sequelize,
+  timestamps: false,
+  underscored: true,
+});
+
+ProjectModel.belongsTo(UserModel, { foreignKey:"a", as:"projects" })
+UserModel.hasMany(ProjectModel, { foreignKey:"a", as:"projects" })
+
+module.exports = {
+  ProjectModel,
+}
