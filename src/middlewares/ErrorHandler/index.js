9e7c127146d1263e5e011d105dc1a67a2d13c6ef
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-08-22 22:18:52 -0300

    :sparkles: creating new instances

---
+++ src/middlewares/ErrorHandler/index.js
@@ -0,0 +1,7 @@
+const { ErrorHandler } = require('./ErrorHandler');
+
+const errorHandler = new ErrorHandler();
+
+module.exports = {
+  errorHandler,
+}
---
+++ src/utils/Hash/index.js
@@ -0,0 +1,7 @@
+const { Hash } = require("./Hash");
+
+const hash = new Hash();
+
+module.exports = {
+	hash,
+};
