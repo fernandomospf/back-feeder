9833b019a0b0d52e92d92a25f90173a2525bff22
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-09-01 20:53:29 -0300

    :build: adding username and classname properties on users migrations and indenting user seed

--- src/database/seeders/20220806212504-create-users-seeder.js
+++ src/database/seeders/20220806212504-create-users-seeder.js
@@ -2,21 +2,29 @@
 
 module.exports = {
   async up(queryInterface, _Sequelize) {
-    await queryInterface.bulkInsert('users', [
-      {
-        name: 'Pedro Lima',
-        email: 'Pedro@email.com',
-        password: '6817a8c319cb881e2e53372baf40320c'
-      },
-      {
-        name: 'Eduado Myauzaki',
-        email: 'oni-chan@email.com',
-        password: '495f4682e38181f6a5a5f3ee8831f1d4'
-      }
-    ], {});
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          name: 'Pedro Lima',
+          username: 'pdin',
+          email: 'Pedro@email.com',
+          password: '6817a8c319cb881e2e53372baf40320c',
+          classname: '16A',
+        },
+        {
+          name: 'Eduado Myauzaki',
+          username: 'Gashlurtz',
+          email: 'oni-chan@email.com',
+          password: '495f4682e38181f6a5a5f3ee8831f1d4',
+          classname: '',
+        },
+      ],
+      {}
+    );
   },
 
   async down(queryInterface, _Sequelize) {
     await queryInterface.bulkDelete('users', null, {});
-  }
+  },
 };
