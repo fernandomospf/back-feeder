11a98b8be26e0ceec9123ff9044c63da6610efd2
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-09-02 06:45:48 -0300

    :build: discomenting route projects

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { projectRoutes } = require('../modules/Project/ProjectRoutes');
 const { userRoutes } = require('../modules/User/UserRoutes');
 
 const routes = express.Router();
 
 routes.use('/users', userRoutes);
-// routes.use('/project');
+routes.use('/projects', projectRoutes);
 
 module.exports = {
   routes,
