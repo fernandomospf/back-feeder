f8abf52b21371c763fcdbdc85151587aa879eef5
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-09-02 02:21:18 -0300

    :build: adding allownull and field properties

--- src/database/migrations/20220806204759-create-projects.js
+++ src/database/migrations/20220806204759-create-projects.js
@@ -3,40 +3,43 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('projects', {
       id: {
+        type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       url: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       gif: {
-        type: Sequelize.BLOB
-      },
-      stack: {
-        type: Sequelize.STRING
+        type: Sequelize.BLOB,
+        allowNull: true,
       },
       module: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       difficult: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       userId: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        field: 'user_id',
         references: {
           model: 'users',
           key: 'id',
-        }
-      }
+        },
+      },
     });
   },
   async down(queryInterface, _Sequelize) {
     await queryInterface.dropTable('projects');
-  }
+  },
 };
