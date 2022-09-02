ab828fac838b67f7b45b565a3cf157f604a17a32
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-09-01 22:01:40 -0300

    :build: creating userRoutes post /register method

--- src/modules/User/UserRoutes.js
+++ src/modules/User/UserRoutes.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const { loginValidate } = require('../../middlewares/LoginValidation');
 const { userController } = require('.');
+const { userValidate } = require('../../middlewares/UserValidation');
 
 const userRoutes = express.Router();
 
 userRoutes
   .post(
-  '/login', (req, res, next) => loginValidate.validate(req, res, next),
-  (req, res) => userController.loginUser(req, res),
-)
+    '/login',
+    (req, res, next) => loginValidate.validate(req, res, next),
+    (req, res) => userController.loginUser(req, res)
+  )
+  .post(
+    '/register',
+    (req, res, next) => userValidate.validate(req, res, next),
+    (req, res) => userController.registerUser(req, res)
+  );
 
 module.exports = {
   userRoutes,
-}
+};
