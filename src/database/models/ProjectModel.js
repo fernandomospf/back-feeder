c80c83c5f246051f11d9105f3fc9f71cb5f44157
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-09-02 18:45:14 -0300

    :refactor: removing ProjectModel relationships

--- src/database/models/ProjectModel.js
+++ src/database/models/ProjectModel.js
@@ -1,8 +1,6 @@
 const { sequelize } = require('.'); 
 const { Model, DataTypes } = require('sequelize');;
 const { UserModel } = require('./UserModel');
-const { StackModel } = require('./StackModel');
-const { StackProjectModel } = require('./StackProjectModel');
 
 class ProjectModel extends Model { }
 
@@ -38,6 +36,10 @@ ProjectModel.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    stacks: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true,
+    },
     userId: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -57,9 +59,6 @@ ProjectModel.init(
 
 UserModel.hasMany(ProjectModel, { foreignKey:'userId', as:'developer' });
 ProjectModel.belongsTo(UserModel, { foreignKey:'userId', as:'userProjects' });
-
-ProjectModel.belongsToMany(StackModel, { through: StackProjectModel, foreignKey:'projectId', as:'projects' });
-StackModel.belongsToMany(ProjectModel, { through: StackProjectModel, foreignKey:'stackId', as:'stacks' });
 
 module.exports = {
   ProjectModel,
